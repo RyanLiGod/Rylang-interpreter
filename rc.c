3b74c464588eafa7b1ea249ae71e8733bc009f17
Ryan Li <1078054388@qq.com>
2019-06-11 16:46:59 +0800

    add simple calc interpreter.

--- rc.c
+++ rc.c
@@ -10,15 +10,18 @@ char *src, *old_src;  // pointer to source code string;
 int poolsize;         // default size of text/data/stack
 int line;             // line number
 
+// 用于词法分析，获取下一个标记，它将自动忽略空白字符
 void next() {
     token = *src++;
     return;
 }
 
+// 用于解析一个表达式
 void expression(int level) {
     // do nothing
 }
 
+// 语法分析的入口，分析整个C语言程序
 void program() {
     next();                  // get next token
     while (token > 0) {
@@ -27,7 +30,7 @@ void program() {
     }
 }
 
-
+// 虚拟机的入口，用于解释目标代码
 int eval() { // do nothing yet
     return 0;
 }
