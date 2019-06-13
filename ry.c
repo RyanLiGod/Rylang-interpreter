7c5cff8989f686bb8d6dbefd4b1b432299be83db
Ryan Li <1078054388@qq.com>
2019-06-13 20:29:19 +0800

    function declaration

--- ry.c
+++ ry.c
@@ -260,6 +260,29 @@ void expression(int level) {
     // do nothing
 }
 
+void function_declaration() {
+    // type func_name (...) {...}
+    //               | this part
+
+    match('(');
+    function_parameter();
+    match(')');
+    match('{');
+    function_body();
+    //match('}');
+
+    // unwind local variable declarations for all local variables.
+    current_id = symbols;
+    while (current_id[Token]) {
+        if (current_id[Class] == Loc) {
+            current_id[Class] = current_id[BClass];
+            current_id[Type]  = current_id[BType];
+            current_id[Value] = current_id[BValue];
+        }
+        current_id = current_id + IdSize;
+    }
+}
+
 void enum_declaration() {
     // parse enum [id] { a = 1, b = 3, ...}
     int i;
